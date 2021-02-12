23b71ad3673350055611106887cf9adfccbf7c6f
Ravindra <ravinavale92@gmail.com>
2021-02-12 08:34:43 +0530

    Initial CMMIT

---
+++ StudentAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = 3;
+            //int m = 5;
+            //int[,] arr = { { 75, 76, 65, 87, 87 }, { 78, 76, 68, 56, 89 }, { 67, 87, 78, 77, 65 } };
+
+            Console.Write("input1: Number of students:\n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("input2: Number of subjects:\n");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[n, m];
+
+            Console.Write("Please Enter per Student per subject Marks:\n\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("[Student:{0},Subject:{1}] Marks = ", i + 1, j + 1);
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            using (Student std = new Student())
+            {
+                string str = std.GetScoreOfStudents(arr, n, m);
+                Console.Write("students total marks after deducting the score for one subject. :\n");
+                Console.Write(str);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ StudentAssignment/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssignment
+{
+    public class Student:IDisposable
+    {
+        public string GetScoreOfStudents(int [,] arr, int n, int m)
+        {
+            try
+            {
+                int minAvg = 0; 
+                int iVal = 0;
+                for (int i = 0; i < m; i++)
+                {
+                    int colTotal = 0; int avg = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        colTotal += arr[j, i];
+                    }
+                    avg = colTotal / n;
+
+                    if (i == 0)
+                    {
+                        minAvg = avg;
+                    }
+                    else
+                    {
+                        if (avg < minAvg)
+                        {
+                            minAvg = avg;
+                            iVal = i;
+                        }
+                    }
+
+                }
+
+                int[] Result = new int[m - 1];
+
+                for (int i = 0; i < n; i++)
+                {
+                    int sum = 0;
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (j != iVal)
+                            sum += arr[i, j];
+                    }
+                    Result[i] = sum;
+                }
+                Array.Sort(Result);
+                Array.Reverse(Result);
+                string outStr = "Output: {";
+                for (int i = 0; i < Result.Length - 1; i++)
+                {
+                    outStr += Result[i] + ",";
+                }
+                outStr = outStr.TrimEnd(',');
+                outStr += "}";
+
+
+                return outStr;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
